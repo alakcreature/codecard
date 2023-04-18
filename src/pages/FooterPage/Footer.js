cf8397f2bc565bab9ecf22efa99e185434ed9da3
webdevexpress <business.webdevexpress@gmail.com>
2023-04-18 19:14:01 +0530

    code cleanup

--- src/pages/FooterPage/Footer.js
+++ src/pages/FooterPage/Footer.js
@@ -1,40 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import staticimages from "../staticImagesLink";
-import './Footer.css';
+import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
-    return (
-        // Footer
-        // Bottom is not responsive
-        <footer className="footer container-fluid">
-            <hr />
-            <div className="footer-inner">
-                <div className="footer-left-side">
-                    <span className="copyright">Copyright © 2021-2022 Codecard</span>
-                    <ul>
-                        {/* <li><Link to="/surprise">Surprise Me</Link></li> */}
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/contactus">Contact Us</Link></li>
-                        <li><Link to="/terms">Terms</Link></li>
-                        <li><Link to="/privacypolicy">Privacy Policy</Link></li>
-                    </ul>
-                </div>
-                <div className="footer-middle">
-                    <p>Made by Alak 
-                    <Link to={{pathname: "https://www.linkedin.com/in/subham-kumar-88593814a/"}} target="_blank">
-                        <FontAwesomeIcon icon={["fab","linkedin"]}/>
-                    </Link>
-                    </p>
-                </div>
-                <div className="footer-right-side">
-                    With <FontAwesomeIcon icon="heart" style={{color:'red'}}/> from
-                    <img src={staticimages.India} alt="India" />
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    // Footer
+    // Bottom is not responsive
+    <footer className="footer container-fluid">
+      <hr />
+      <div className="footer-inner">
+        <div className="footer-left-side">
+          <span className="copyright">Copyright © 2021-2023 Codecard</span>
+          <ul>
+            {/* <li><Link to="/surprise">Surprise Me</Link></li> */}
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contactus">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/terms">Terms</Link>
+            </li>
+            <li>
+              <Link to="/privacypolicy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-middle">
+          <p>
+            Made by Alak
+            <Link
+              to={{
+                pathname: "https://www.linkedin.com/in/subham-kumar-88593814a/",
+              }}
+              target="_blank">
+              <FontAwesomeIcon icon={["fab", "linkedin"]} />
+            </Link>
+          </p>
+        </div>
+        <div className="footer-right-side">
+          With <FontAwesomeIcon icon="heart" style={{ color: "red" }} /> from
+          <img src={staticimages.India} alt="India" />
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
